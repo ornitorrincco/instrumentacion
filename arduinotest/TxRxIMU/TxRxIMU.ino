4c9c9aabee7adbb931de19e14cb90ad393e59395
Abraham Alonzo Velazquez <ornitorrincco@gmail.com>
2017-09-21 03:58:52 -0700

    add Tx/Rx Comunication with IMU Razor 9DOF

---
+++ arduinotest/TxRxIMU/TxRxIMU.ino
@@ -0,0 +1,21 @@
+String sensorData;
+char c;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("9DOF Test"); 
+}
+  
+void loop() {
+  while (Serial.available()) {  
+    if (Serial.available() >0) {
+   c = Serial.read(); 
+        //sensorData += c; //adds c to the sensorData string
+       Serial.print(c); //see whatever came in on Serial.read()
+    }
+  }
+  if (sensorData.length() >0) {
+   Serial.println(sensorData);  //output the string
+   Serial.readString()=""; //resets reString for the next iteration of the loop
+  } 
+} 
+
