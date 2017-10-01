fb289d0c9ac64f9168cc356c8f9ffe84fa18d0c8
Abraham Alonzo Velazquez <ornitorrincco@gmail.com>
2017-09-30 18:57:22 -0700

    Correct clean data method added

--- blender/dOFRazorSerialReader.py
+++ blender/dOFRazorSerialReader.py
@@ -1,31 +1,27 @@
 import serial
 from time import sleep
 
-
-def cleanInput(data_string):
-    # if is not a string
-    dataParsed = str(data_string)
-    number = ''
-    # get all the number of the string an concatenate them
-    for character in dataParsed:
-        if character.isdigit():
-            number += character
-
-    # return a string
-    return number
-
-
 arduino = serial.Serial('COM3', 9600)
 accelerometer = []
 giroscope = []
 magnetoscope = []
-while True:
-    data = arduino.readline()
-    data_DOF = str(data)
-    giroscope = data_DOF.split(',')[0:3]
-    eaccelerometer = data_DOF.split(',')[3:6]
-    magnetoscope = data_DOF.split(',')[6:9]
+data_array = []
 
-    #data_clean = cleanInput(data)
-    #outputString = "acelerometer = " + accelerometer + ' giroscope = ' + giroscope + ' magnetoscope = ' + magnetoscope
-    print(giroscope)
+
+def dOF9Razor(data):
+    data_DOF = str(data).split(',')
+    if len(data_DOF) == 9:
+        giroscope = data_DOF[0:3]
+        giroscope[0] = giroscope[0].split('$')[1]
+        accelerometer = data_DOF[3:6]
+        magnetoscope = data_DOF[6:9]
+        magnetoscope[2] = magnetoscope[2].split("#")[0]
+        # outputString = "acelerometer = " + accelerometer + ' giroscope = ' + giroscope + ' magnetoscope = ' + magnetoscope
+        return accelerometer, giroscope, magnetoscope
+
+while True:
+    data_string = arduino.readline()
+    try:
+        acceleration, degree, magnetoscope = dOF9Razor(data_string)
+    except:
+        pass
