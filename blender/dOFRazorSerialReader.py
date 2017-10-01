160b6e2561a0699b7a41d3b55b340c24ea025b66
Abraham Alonzo Velazquez <ornitorrincco@gmail.com>
2017-09-30 18:06:29 -0700

    workflow added and method of data cleaning 9DOF  in Progress

--- blender/dOFRazorSerialReader.py
+++ blender/dOFRazorSerialReader.py
@@ -16,9 +16,16 @@ def cleanInput(data_string):
 
 
 arduino = serial.Serial('COM3', 9600)
+accelerometer = []
+giroscope = []
+magnetoscope = []
 while True:
     data = arduino.readline()
     data_DOF = str(data)
-    data_DOF = data_DOF.split(',')[0]
+    giroscope = data_DOF.split(',')[0:3]
+    eaccelerometer = data_DOF.split(',')[3:6]
+    magnetoscope = data_DOF.split(',')[6:9]
+
     #data_clean = cleanInput(data)
-    print(data_DOF)
+    #outputString = "acelerometer = " + accelerometer + ' giroscope = ' + giroscope + ' magnetoscope = ' + magnetoscope
+    print(giroscope)
